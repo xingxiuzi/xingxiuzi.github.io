8b467e9c8e5560614b5f567f48fa862f12341035
Yuzhi <qingyuzhi445019936@gmail.com>
2023-11-10 22:06:01 +0800

    2rd commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,48 @@ import iconLink from './images/external-link.png'
 import ascend from './images/ascend.png'
 import kunpeng from './images/kunpeng.png'
 import ninjable from './images/ninjable.png'
+import fictional from './images/fictional.jpg'
+import js from './images/java-script.png'
+import html from './images/html-5.png'
+import css from './images/css-3.png'
+import wordpress from './images/wordpress.png'
+import sass from './images/sass.png'
+import Tailwind from './images/tailwind-css.svg'
+import php from './images/php.png'
+import down from './images/down.png'
+import location from './images/location.png'
+import email from './images/email.png'
 
 
 function App() {
   return (
-    <div className="App px-8 font-mono">
+    <div className="App font-mono overflow-hiden">
       <Header></Header>
       <Introducing></Introducing>
       <TechStack></TechStack>
       <About></About>
       <Project></Project>
       <ContactMe></ContactMe>
+      <Footer></Footer>
     </div>
   );
 }
 
 function Header(){
+  function handleClick(ele){
+    const container = document.querySelector('html')
+    const target =document.querySelector(`#${ele}`).offsetTop
+    container.scrollTo({top: target, behavior: 'smooth'})
+  }
   return(
-    <div className='h-16 flex justify-between text-xl'>
-      <div className='items-center flex hover:font-extrabold cursor'>Yuzhi.Dev</div>
-      <ul className='menuList flex flex-row-reverse h-full items-center'>
-        <li className='hover:font-semibold'><a href='#projectList'>Contact</a></li>
-        <li className='mr-4 hover:font-semibold'><a href='#projectList'>Projects</a></li>
-        <li className='mr-4 hover:font-semibold'><a href='#projectList'>About</a></li>
-        <li className="flex justify-center mr-4 hover:font-semibold"><a href='#javascript'>Home</a></li>
-      </ul>
+    <div className='px-8 h-16 flex justify-between text-xl fixed header w-full'>
+        <div className='items-center flex hover:font-extrabold cursor'>Yuzhi.Dev</div>
+        <ul className='menuList flex flex-row-reverse h-full items-center'>
+          <li className='hover:font-semibold cursor-pointer' onClick={()=>handleClick('Contact')}>Contact</li>
+          <li className='mr-4 hover:font-semibold cursor-pointer' onClick={()=>handleClick('Projects')}>Projects</li>
+          <li className='mr-4 hover:font-semibold cursor-pointer' onClick={()=>handleClick('About')}>About</li>
+          <li className="flex justify-center mr-4 hover:font-semibold"><a href='#'>Home</a></li>
+        </ul>
     </div>
   ) 
 }
@@ -57,10 +74,18 @@ function Project(){
       description:'Ninjable is a ninja game theme programmer learning website, where you can strengthen your "ninja" by learning development skills.',
       techAry:['HTML + CSS','GraphQL']
 
+    },
+    {
+      coverImg:fictional,
+      url:'',
+      title:'Fictional University',
+      description:'"Fictional University" is a virtual project built in a paid WordPress course on Udemy, designed to practice using relevant features of WordPress, such as plugins, custom API interfaces, etc.      ',
+      techAry:['Wordpress','PHP']
+
     }
   ]
   return(
-      <div className='mt-72 flex justify-center'>
+      <div className='mt-72 flex justify-center' id='Projects'>
         <div className='basis-2/3 '>
           <p className='text-start basis-2/3 text-sky-600 font-semibold'>PROTFOLIO</p>
           <p className='text-start basis-2/3 text-black font-black text-lg'>Each projects is a unique piece of development</p>
@@ -88,10 +113,7 @@ function Project(){
                     <div className='text-black font-black text-lg text-center'>{item.title}</div>
                     <div className='mt-8 text-center'>{item.description}</div>
                     <div className='mt-8 text-center'><RenderTech techAry={item.techAry} /></div>
-                    <a href={item.url} className='flex items-center justify-center hover:text-sky-600 mt-8'>
-                      Live Demo
-                      <img src={iconLink} alt='' className='w-4 h-4 ml-2'/>
-                    </a>
+                    <Live url={item.url}></Live>
                   </div>
                   <img src='' alt='' className='basis-2/3 overflow-hiden rounded-3xl shadow-xl bg-cover bg-top max-h-96' style={{backgroundImage:`url(${item.coverImg})`}}/>
                 </li>
@@ -117,7 +139,7 @@ function RenderTech(props){
 
 function About(){
   return(
-    <div className='mt-72 flex justify-center'>
+    <div className='mt-72 flex justify-center' id='About'>
       <div className='basis-2/3 flex'>
         <div className='basis-2/5 workTable h-60 bg-cover bg-center rounded-lg relative'>
           <Animation></Animation>
@@ -125,7 +147,7 @@ function About(){
         <div className='basis-3/5 ml-16'>
           <div className='text-sky-600 font-semibold'>ABOUT ME</div>
           <div className='text-black font-black text-lg'>A dedicated Front-end Developer based in China,Chengdu.</div>
-          <div className='text-sm mt-4'>As a Front-end Developer,I possess an impressive arsenal of skills in HTML,CSS,JavaScript,React,Vue,Tailwind,and SCSS.I excel in designing and maintaining responsive websites that offer a smooth user experience.My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</div>
+          <div className='text-sm mt-4'>As a Front-end Developer,I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Vue, Tailwind, and SCSS.I excel in designing and maintaining responsive websites that offer a smooth user experience.My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</div>
         </div>
       </div>
     </div>
@@ -134,7 +156,7 @@ function About(){
 
 function Introducing(){
   return(
-    <div className='mt-36 flex justify-center'>
+    <div className=' flex justify-center pt-56'>
       <div className='basis-1/3 pt-8'>
         <p className='text-4xl font-semibold'>Front-end Developer</p>
         <span>Hi,I'm Yuzhi Qing. A passionate Front-end Developer based in China,Chengdu.</span>
@@ -151,12 +173,13 @@ function TechStack(){
     <div className='flex justify-center mt-32'>
       <div className='basis-2/3 flex justify-start	items-center'>
         <span className='font-semibold'>Tech Stack |</span>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-24'/>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-12'/>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-12'/>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-12'/>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-12'/>
-        <img src='https://r.huaweistatic.com/s/ascendstatic/lst/as/doc/pc/img_doc_2210211.png' alt='' className='w-8 h-8 ml-12'/>
+        <img src={css} alt='' className='w-8 h-8 ml-12'/>
+        <img src={html} alt='' className='w-8 h-8 ml-12'/>
+        <img src={js} alt='' className='w-8 h-8 ml-12'/>
+        <img src={php} alt='' className='w-8 h-8 ml-12'/>
+        <img src={wordpress} alt='' className='w-8 h-8 ml-12'/>
+        <img src={sass} alt='' className='w-8 h-8 ml-12'/>
+        <img src={Tailwind} alt='' className='w-8 h-8 ml-12'/>
       </div>
     </div>
   )
@@ -196,13 +219,53 @@ function Animation(){
 
 function ContactMe(){
   return(
-    <div className='flex justify-center'>
-      <div className='basis-2/3 mt-72 flex '>
+    <div className='flex justify-center' id='Contact'>
+      <div className='basis-2/3 mt-72 flex flex-col'>
         <p className='text-start text-sky-600 font-semibold'>Contact</p>
-        <p>Don't be shy! Hit me up!</p>
-        <div></div>
+        <p className='flex'>Don't be shy! Hit me up! <img className='w-8 h-8' src={down} alt=''/></p>
+        <div className='mt-24 flex'>
+          <div className='flex'>
+            <img className='w-16 h-16' src={location} alt=''/>
+            <div className='ml-8 flex flex-col justify-between'>
+              <p className='text-sky-600 font-semibold'>location</p>
+              <p>Chengdu, China</p>
+            </div>
+          </div>
+          <div className='flex ml-24'>
+            <img className='w-16 h-16' src={email} alt=''/>
+            <div className='ml-8 flex flex-col justify-between'>
+              <p className='text-sky-600 font-semibold'>mail</p>
+              <p>qingyuzhi445019936@gmail.com</p>
+            </div>
+          </div>
+        </div>
         <div></div>
       </div>
+    </div>
+  )
+}
+
+function Live(props){
+  if(props.url){
+    return(
+      <a href={props.url} className='flex items-center justify-center hover:text-sky-600 mt-8'>
+        Live Demo
+        <img src={iconLink} alt='' className='w-4 h-4 ml-2'/>
+      </a>
+    )
+  }else{
+    return(
+      <p className='flex items-center justify-center mt-8 font-semibold'>
+        Coming Soon 
+      </p>
+    )
+  }
+}
+
+function Footer(){
+  return(
+    <div className='mt-24 rounded-lg bg-black text-white flex justify-center pt-12 pb-12'>
+      <div className='basis-2/3'>copyright©2023. All rights are reserved</div>
     </div>
   )
 }
